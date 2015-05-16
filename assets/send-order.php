35cc817220f7c2f1fc5f6bf94e3f338e8f4ad8ed
denysboiko <denys.bojko@gmail.com>
2015-05-16 17:55:17 +0300

    new php-send

--- assets/send-order.php
+++ assets/send-order.php
@@ -1,11 +1,44 @@
 <?php
 
+    $name = $_POST["client-name"];
+    $phone = $_POST["phone-num"];
+
+    $from = "Woodluck Support <support@woodluck.com.ua>";
     $to = 'denys.bojko@gmail.com';
     $subject = 'Заказ на перезвон';
-    $message = "Имя: ".$_POST["client-name"]."\n"."Телефон: ".$_POST["phone-num"];
-    $headers = 'From: Woodluck Support <support@woodluck.com.ua/>' . "\r\n";
+    $body = "Имя: ".$name."\n"."Телефон: ".$phone;
+
+
+    $host = "customers.freehosting.com";
+    $username = "support@woodluck.com.ua";
+    $password = "8kHjU12p6u";
+
+
+    $headers = array ('From' => $from,
+           'To' => $to,
+           'Subject' => $subject);
+         $smtp = Mail::factory('smtp',
+           array ('host' => $host,
+             'auth' => true,
+             'username' => $username,
+             'password' => $password));
+
+         $mail = $smtp->send($to, $headers, $body);
+
+         if (PEAR::isError($mail)) {
+           echo("<p>" . $mail->getMessage() . "</p>");
+          } else {
+           echo("Мы вам скоро перезвоним.");
+          }
+
+    /*$headers = 'From: Woodluck Support <support@woodluck.com.ua/>' . "\r\n";
  
     mail($to, $subject, $message, $headers);
-    echo "Мы вам скоро перезвоним.";
-	
+    echo "Мы вам скоро перезвоним.";*/
+
+
+
+
+
+
 ?>
