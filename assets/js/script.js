67258c725d7f712197a7c4e74cf0a8e0c794dfaf
Denys Boiko <denysboiko@users.noreply.github.com>
2018-10-17 13:50:30 +0300

    caarousel fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,39 +77,6 @@ $('#nav a, .cta-nav a').click(function(){
     $('body,html').animate({scrollTop: pos},700);
 });
 
-/* copy loaded thumbnails into carousel */
-$('.panel .img-responsive').on('load', function() {}).each(function(i) {
-    if(this.complete) {
-        var item = $('<div class="item"></div>');
-        var itemDiv = $(this).parent('a');
-        var title = $(this).parent('a').attr("title");
-
-        item.attr("title",title);
-        $(itemDiv.html()).appendTo(item);
-        item.appendTo('#modalCarousel .carousel-inner');
-        if (i==0){ // set first item active
-            item.addClass('active');
-        }
-    }
-});
-
-/* change modal title when slide changes */
-$('#modalCarousel').on('slid.bs.carousel', function () {
-    $('.modal-title').html($(this).find('.active').attr("title"));
-});
-
-/* when clicking a thumbnail */
-$('.panel-thumbnail > a').click(function(e){
-
-    e.preventDefault();
-    var idx = $(this).parents('.panel').parent().index();
-    var id = parseInt(idx);
-
-    $('#myModal').modal('show'); // show the modal
-    $('#modalCarousel').carousel(id); // slide carousel to selected
-    return false;
-});
-<!--SCROLL TO TOP-->
 $(document).ready(function(){
     // fade in and fade out
     $(function () {
@@ -131,4 +98,3 @@ $(document).ready(function(){
     });
 
 });
-<!--END-->
