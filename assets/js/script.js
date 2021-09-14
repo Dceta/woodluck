8576d1c5b9137d99286fd644e98c8b77ff424e1a
Denys Boiko <denys.boiko@gmail.com>
2021-09-14 22:35:14 +0300

    Update form send logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 $('.carousel').carousel({interval: false});
 
-$('.navbar-nav a').click(function() {
+$('.navbar-nav a').click(function () {
     $('.logo').collapse();
 });
 $('#myTab a').click(function (e) {
@@ -9,27 +9,28 @@ $('#myTab a').click(function (e) {
 });
 
 const handleSubmit = (e) => {
-  e.preventDefault()
-  $('#send').addClass('btn-disabled');
-  $('#send').prop('disabled', true);
-  $('.loader').show();
-  $('#send > span').html("");
-  let myForm = document.getElementById('send-form');
-  let formData = new FormData(myForm)
-  fetch('/', {
-    method: 'POST',
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString()
-  }).then(() => {
-      $('.loader').delay(2000).hide(function(){
-                    $('#send > span').html("Спасибо!");
-                    $('#success').html(response).fadeIn();
-                    $('#success').delay(2000).fadeOut();
-                }).delay(2000).done(function () {
-                $('#send').removeClass('btn-disabled');
-                $('#send').prop('disabled', false);
-            });;
-  })
+    e.preventDefault()
+    $('#send').addClass('btn-disabled');
+    $('#send').prop('disabled', true);
+    $('.loader').show();
+    $('#send > span').html("");
+    let myForm = document.getElementById('send-form');
+    let formData = new FormData(myForm)
+    fetch('/', {
+        method: 'POST',
+        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        body: new URLSearchParams(formData).toString()
+    }).then(() => {
+        $('.loader')
+            .delay(2000)
+            .hide(function () {
+                $('#send > span').html("Спасибо!");
+                $('#success').html(response).fadeIn();
+                $('#success').delay(2000).fadeOut();
+            });
+        $('#send').removeClass('btn-disabled');
+        $('#send').prop('disabled', false);
+    })
 }
 
 $(document).ready(function () {
@@ -37,21 +38,21 @@ $(document).ready(function () {
 });
 
 /* highlight the top nav as scrolling occurs */
-$('body').scrollspy({ target: '#nav' });
+$('body').scrollspy({target: '#nav'});
 
 /* smooth scrolling for scroll to top */
-$('.scroll-top').click(function(){
-    $('body,html').animate({scrollTop:0},1000);
+$('.scroll-top').click(function () {
+    $('body,html').animate({scrollTop: 0}, 1000);
 });
 
 /* smooth scrolling for nav sections */
-$('#nav a, .cta-nav a').click(function(){
+$('#nav a, .cta-nav a').click(function () {
     var link = $(this).attr('href');
     var pos = $(link).offset().top;
-    $('body,html').animate({scrollTop: pos},700);
+    $('body,html').animate({scrollTop: pos}, 700);
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     // fade in and fade out
     $(function () {
         $(window).scroll(function () {
